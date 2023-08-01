ee3bfbcb75edc82550c41db1dac9b8e63a4b73ee
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2023-08-01 15:15:14 -0600

    estilos en login

--- src/app/pages/admin-panel/login/login.component.ts
+++ src/app/pages/admin-panel/login/login.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { UsuariosService } from '@services/usuarios.service';
+import { MessageService } from 'primeng/api';
 import { Login } from '@models/login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  providers: [MessageService],
 })
 export class LoginComponent {
   login: Login = {
@@ -18,11 +19,13 @@ export class LoginComponent {
 
   loginForm!: FormGroup;
 
+  passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+
   constructor(
     private formBuilder: FormBuilder,
     private usuarioSvc: UsuariosService,
-    private toastr: ToastrService,
-    private router: Router
+    private router: Router,
+    private messageSvc: MessageService
   ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -54,15 +57,23 @@ export class LoginComponent {
         localStorage.setItem('access_token', res.token);
         // localStorage.setItem('refresh_token', '');
         localStorage.setItem('usuario', JSON.stringify(res.usuario));
-        this.toastr.success(
-          `Bienvenido ${res.usuario.nombre}`,
-          'Login exitoso'
-        );
-        this.router.navigate(['/admin/dashboard']).then((r) => r);
+        this.router.navigate(['/admin/dashboard']);
       },
       (err) => {
         console.log(err);
-        this.toastr.error(err.error, 'Error al iniciar sesión');
+        this.messageSvc.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al iniciar sesión',
+        });
+      },
+      () => {
+        console.log('Petición completa');
+        this.messageSvc.add({
+          severity: 'success',
+          summary: 'Bienvenido a Clamaroj',
+          detail: `Hola ${this.login.correo}`,
+        });
       }
     );
   }
