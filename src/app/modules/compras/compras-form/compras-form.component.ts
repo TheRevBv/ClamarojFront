73b9de040b162d76c4bc88c3567fac019a536973
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2023-08-08 15:47:19 -0600

    Agrega configuracion de no agregar test-file
    Agrega routeo de ventas y compras
    Actualiza semantizacion en materias primas

---
+++ src/app/modules/compras/compras-form/compras-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compras-form',
+  templateUrl: './compras-form.component.html',
+  styleUrls: ['./compras-form.component.css']
+})
+export class ComprasFormComponent {
+
+}
--- src/app/modules/ventas/ventas-routing.module.ts
+++ src/app/modules/ventas/ventas-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VentasListComponent } from './ventas-list/ventas-list.component';
+import { VentasFormComponent } from './ventas-form/ventas-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VentasListComponent,
+    children: [
+      {
+        path: 'detalle',
+        component: VentasFormComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VentasRoutingModule { }
+export class VentasRoutingModule {}
